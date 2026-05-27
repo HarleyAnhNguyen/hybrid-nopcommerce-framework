e68e00992a1e492b74f6cdfd8aeb34b7d12c2474
HarleyAnhNguyen <alohaitit@gmail.com>
2026-05-27 10:34:46 +0700

    Multiple Browser I

--- action/commons/BaseTest.java
+++ action/commons/BaseTest.java
@@ -1,8 +1,35 @@
 package commons;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
 import java.util.Random;
 
 public class BaseTest {
+    private WebDriver driver;
+    protected  WebDriver getBrowserDriver(String browserName){
+        switch (browserName){
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new RuntimeException("Browser name is invalid");
+        }
+        driver.get("http://localhost:8086/");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+        return driver;
+    }
     protected int generateRandomNumber(){
         return  new Random().nextInt(99999);
     }
---
+++ testcases/com/nopcommerce/users/Level_04_Multiple_Browser.java
@@ -0,0 +1,101 @@
+package com.nopcommerce.users;
+
+import commons.BaseTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pageObjects.CustomerInfoPageObject;
+import pageObjects.HomePageObject;
+import pageObjects.LoginPageObject;
+import pageObjects.RegisterPageObject;
+
+import java.time.Duration;
+
+public class Level_04_Multiple_Browser extends BaseTest {
+    private WebDriver driver;
+    private HomePageObject homePage;
+    private RegisterPageObject registerPage;
+    private LoginPageObject loginPage;
+    private CustomerInfoPageObject customerInfoPage;
+    private String firstName, middleName, lastName,email,companyName,password;
+
+
+    @Parameters("browser")
+    @BeforeClass
+    public void beforeClass(String browserName){
+        driver = getBrowserDriver(browserName);
+        // Page do duoc sinh ra va bat dau lam nhung action cuar page do
+        homePage = new HomePageObject(driver);
+        firstName = "Bo";
+        middleName = "Van";
+        lastName = "Bap";
+        email="bobapthui" + generateRandomNumber() + "@gmail.com";
+        companyName="bobapthui";
+        password = "1234567";
+
+    }
+
+    @Test
+    public void User_01_Register(){
+       //Action 1
+       homePage.clickToRegisterLink();
+
+       //Tu HomePage -> RegisterPage
+       // Page do duoc sinh ra va bat dau lam nhung action cuar page do
+       registerPage = new RegisterPageObject(driver);
+       registerPage.clickToGenderRadioButton();
+       registerPage.enterToFirstNameTextbox(firstName);
+       registerPage.enterToLastNameTextbox(lastName);
+       registerPage.enterToEmailTextbox(email);
+       registerPage.enterToCompanyTextbox(companyName);
+       registerPage.enterToPasswordTextbox(password);
+       registerPage.enterToConfirmPasswordTextbox(password);
+       registerPage.clickToRegisterButton();
+
+       Assert.assertEquals(registerPage.getRegisterSuccessMessage(), "Your registration completed");
+
+       registerPage.clickToLogOutButton();
+    }
+
+    @Test
+    public void User_02_Login(){
+        homePage.clickToLoginLink();
+
+        loginPage = new LoginPageObject(driver);
+        loginPage.enterToEmailTextbox(email);
+        loginPage.enterToPasswordTextbox(password);
+        loginPage.clickToLoginButton();
+
+        Assert.assertTrue(homePage.isMyAccountLinkDisplayed());
+    }
+
+
+
+    @Test
+    public void User_03_MyAccount(){
+        homePage.clickToMyAccountLink();
+
+        customerInfoPage = new CustomerInfoPageObject(driver);
+
+        Assert.assertTrue(customerInfoPage.isGenderSelected());
+        Assert.assertEquals(customerInfoPage.getFirstNameTextboxValue(), firstName);
+        Assert.assertEquals(customerInfoPage.getLastNameTextboxValue(), lastName);
+        Assert.assertEquals(customerInfoPage.getEmailTextboxValue(), email);
+        Assert.assertEquals(customerInfoPage.getCompanyTextboxValue(), companyName);
+
+    }
+
+    @AfterClass
+    public void afterClass(){
+         driver.quit();
+
+    }
+
+
+}
